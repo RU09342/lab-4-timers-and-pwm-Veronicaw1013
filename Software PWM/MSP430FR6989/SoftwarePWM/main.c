9ed4f077e0e9637458ba87812e2415c9b99c117b
Veronicaw1013 <31421648+Veronicaw1013@users.noreply.github.com>
2017-10-17 22:20:33 -0400

    Update main.c

--- Software PWM/MSP430FR6989/SoftwarePWM/main.c
+++ Software PWM/MSP430FR6989/SoftwarePWM/main.c
@@ -1,4 +1,4 @@
-#include <msp430.h>
+ #include <msp430.h>
 
 int main(void){
 
@@ -27,7 +27,7 @@ int main(void){
 
 #pragma vector=TIMER0_B1_VECTOR
 __interrupt void Timer0_B1_ISR (void){
-    if(TB0CCR1==500){
+    if(TB0CCR1==500){ // if CCR1 reaches max value
     }
     else{
         P1OUT &= ~BIT0; // LED off
@@ -51,7 +51,7 @@ __interrupt void Port_1(void){
         P9OUT ^= BIT7;              // flip other led
         TB0CCR1 += 50;              // increase duty by 10%
         if(TB0CCR1 == 550)
-            TB0CCR1 = 0;            // stay on if at 100%
+            TB0CCR1 = 0;            
     }
     else if (P1IES & BIT1){      // executes on the negedge
         P9OUT &= ~BIT7;
